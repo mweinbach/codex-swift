dd7ea6f9ed8c09292bb7cb57e150f89cd3478976
mweinbach <maxweinbach5@gmail.com>
2026-05-14 01:23:44 -0400

    Add marketplace protocol models

--- Sources/CodexCore/AppServerPluginProtocol.swift
+++ Sources/CodexCore/AppServerPluginProtocol.swift
@@ -1,5 +1,138 @@
 import Foundation
 
+public struct MarketplaceAddParams: Equatable, Sendable {
+    public let source: String
+    public let refName: String?
+    public let sparsePaths: [String]?
+
+    private enum CodingKeys: String, CodingKey {
+        case source
+        case refName
+        case sparsePaths
+    }
+
+    public init(source: String, refName: String? = nil, sparsePaths: [String]? = nil) {
+        self.source = source
+        self.refName = refName
+        self.sparsePaths = sparsePaths
+    }
+}
+
+extension MarketplaceAddParams: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        source = try container.decode(String.self, forKey: .source)
+        refName = try container.decodeIfPresent(String.self, forKey: .refName)
+        sparsePaths = try container.decodeIfPresent([String].self, forKey: .sparsePaths)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(source, forKey: .source)
+        try container.encodeNilOrValue(refName, forKey: .refName)
+        try container.encodeNilOrValue(sparsePaths, forKey: .sparsePaths)
+    }
+}
+
+public struct MarketplaceAddResponse: Equatable, Codable, Sendable {
+    public let marketplaceName: String
+    public let installedRoot: AbsolutePath
+    public let alreadyAdded: Bool
+
+    public init(marketplaceName: String, installedRoot: AbsolutePath, alreadyAdded: Bool) {
+        self.marketplaceName = marketplaceName
+        self.installedRoot = installedRoot
+        self.alreadyAdded = alreadyAdded
+    }
+}
+
+public struct MarketplaceRemoveParams: Equatable, Codable, Sendable {
+    public let marketplaceName: String
+
+    public init(marketplaceName: String) {
+        self.marketplaceName = marketplaceName
+    }
+}
+
+public struct MarketplaceRemoveResponse: Equatable, Sendable {
+    public let marketplaceName: String
+    public let installedRoot: AbsolutePath?
+
+    public init(marketplaceName: String, installedRoot: AbsolutePath? = nil) {
+        self.marketplaceName = marketplaceName
+        self.installedRoot = installedRoot
+    }
+}
+
+extension MarketplaceRemoveResponse: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case marketplaceName
+        case installedRoot
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        marketplaceName = try container.decode(String.self, forKey: .marketplaceName)
+        installedRoot = try container.decodeIfPresent(AbsolutePath.self, forKey: .installedRoot)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(marketplaceName, forKey: .marketplaceName)
+        try container.encodeNilOrValue(installedRoot, forKey: .installedRoot)
+    }
+}
+
+public struct MarketplaceUpgradeParams: Equatable, Sendable {
+    public let marketplaceName: String?
+
+    public init(marketplaceName: String? = nil) {
+        self.marketplaceName = marketplaceName
+    }
+}
+
+extension MarketplaceUpgradeParams: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case marketplaceName
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        marketplaceName = try container.decodeIfPresent(String.self, forKey: .marketplaceName)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encodeNilOrValue(marketplaceName, forKey: .marketplaceName)
+    }
+}
+
+public struct MarketplaceUpgradeResponse: Equatable, Codable, Sendable {
+    public let selectedMarketplaces: [String]
+    public let upgradedRoots: [AbsolutePath]
+    public let errors: [MarketplaceUpgradeErrorInfo]
+
+    public init(
+        selectedMarketplaces: [String],
+        upgradedRoots: [AbsolutePath],
+        errors: [MarketplaceUpgradeErrorInfo]
+    ) {
+        self.selectedMarketplaces = selectedMarketplaces
+        self.upgradedRoots = upgradedRoots
+        self.errors = errors
+    }
+}
+
+public struct MarketplaceUpgradeErrorInfo: Equatable, Codable, Sendable {
+    public let marketplaceName: String
+    public let message: String
+
+    public init(marketplaceName: String, message: String) {
+        self.marketplaceName = marketplaceName
+        self.message = message
+    }
+}
+
 public enum PluginShareDiscoverability: String, Codable, Equatable, Sendable {
     case listed = "LISTED"
     case unlisted = "UNLISTED"
--- Tests/CodexCoreTests/AppServerPluginProtocolTests.swift
+++ Tests/CodexCoreTests/AppServerPluginProtocolTests.swift
@@ -2,6 +2,92 @@ import CodexCore
 import XCTest
 
 final class AppServerPluginProtocolTests: XCTestCase {
+    func testMarketplaceAddParamsEncodeExplicitNullOptionalsLikeRustProtocol() throws {
+        let params = MarketplaceAddParams(source: "openai/debug-marketplace")
+
+        try XCTAssertJSONObjectEqual(params, [
+            "source": "openai/debug-marketplace",
+            "refName": NSNull(),
+            "sparsePaths": NSNull()
+        ])
+
+        let decoded = try JSONDecoder().decode(
+            MarketplaceAddParams.self,
+            from: Data(#"{"source":"openai/debug-marketplace","refName":"main","sparsePaths":["plugins/debug"]}"#.utf8)
+        )
+
+        XCTAssertEqual(decoded.source, "openai/debug-marketplace")
+        XCTAssertEqual(decoded.refName, "main")
+        XCTAssertEqual(decoded.sparsePaths, ["plugins/debug"])
+    }
+
+    func testMarketplaceResponseShapesMatchRustProtocol() throws {
+        try XCTAssertJSONObjectEqual(
+            MarketplaceAddResponse(
+                marketplaceName: "debug",
+                installedRoot: try AbsolutePath(absolutePath: "/tmp/marketplaces/debug"),
+                alreadyAdded: false
+            ),
+            [
+                "marketplaceName": "debug",
+                "installedRoot": "/tmp/marketplaces/debug",
+                "alreadyAdded": false
+            ]
+        )
+
+        try XCTAssertJSONObjectEqual(MarketplaceRemoveParams(marketplaceName: "debug"), [
+            "marketplaceName": "debug"
+        ])
+
+        try XCTAssertJSONObjectEqual(MarketplaceRemoveResponse(marketplaceName: "debug"), [
+            "marketplaceName": "debug",
+            "installedRoot": NSNull()
+        ])
+
+        try XCTAssertJSONObjectEqual(
+            MarketplaceRemoveResponse(
+                marketplaceName: "debug",
+                installedRoot: try AbsolutePath(absolutePath: "/tmp/marketplaces/debug")
+            ),
+            [
+                "marketplaceName": "debug",
+                "installedRoot": "/tmp/marketplaces/debug"
+            ]
+        )
+    }
+
+    func testMarketplaceUpgradeParamsAndResponseRoundTripLikeRustProtocol() throws {
+        try XCTAssertJSONObjectEqual(MarketplaceUpgradeParams(), [
+            "marketplaceName": NSNull()
+        ])
+        try XCTAssertJSONObjectEqual(MarketplaceUpgradeParams(marketplaceName: "debug"), [
+            "marketplaceName": "debug"
+        ])
+
+        let response = MarketplaceUpgradeResponse(
+            selectedMarketplaces: ["debug"],
+            upgradedRoots: [try AbsolutePath(absolutePath: "/tmp/marketplaces/debug")],
+            errors: [
+                MarketplaceUpgradeErrorInfo(marketplaceName: "broken", message: "boom")
+            ]
+        )
+
+        try XCTAssertJSONObjectEqual(response, [
+            "selectedMarketplaces": ["debug"],
+            "upgradedRoots": ["/tmp/marketplaces/debug"],
+            "errors": [
+                ["marketplaceName": "broken", "message": "boom"]
+            ]
+        ])
+
+        let decoded = try JSONDecoder().decode(
+            MarketplaceUpgradeParams.self,
+            from: Data(#"{"marketplaceName":null}"#.utf8)
+        )
+
+        XCTAssertNil(decoded.marketplaceName)
+    }
+
     func testPluginShareSaveParamsEncodeExplicitNullOptionalsLikeRustProtocol() throws {
         let params = PluginShareSaveParams(pluginPath: try AbsolutePath(absolutePath: "/repo/plugin"))
 
