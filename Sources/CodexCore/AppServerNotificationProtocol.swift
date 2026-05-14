5eebb0018e50397ebe8722a107355e895e886480
mweinbach <maxweinbach5@gmail.com>
2026-05-14 04:58:07 -0400

    Add Guardian review notification payloads

--- Sources/CodexCore/AppServerNotificationProtocol.swift
+++ Sources/CodexCore/AppServerNotificationProtocol.swift
@@ -61,6 +61,335 @@ public struct GuardianWarningNotification: Equatable, Codable, Sendable {
     }
 }
 
+public enum GuardianApprovalReviewStatus: String, Codable, Equatable, Sendable {
+    case inProgress
+    case approved
+    case denied
+    case timedOut
+    case aborted
+}
+
+public struct GuardianApprovalReview: Equatable, Sendable {
+    public let status: GuardianApprovalReviewStatus
+    public let riskLevel: GuardianRiskLevel?
+    public let userAuthorization: GuardianUserAuthorization?
+    public let rationale: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case status
+        case riskLevel
+        case userAuthorization
+        case rationale
+    }
+
+    public init(
+        status: GuardianApprovalReviewStatus,
+        riskLevel: GuardianRiskLevel? = nil,
+        userAuthorization: GuardianUserAuthorization? = nil,
+        rationale: String? = nil
+    ) {
+        self.status = status
+        self.riskLevel = riskLevel
+        self.userAuthorization = userAuthorization
+        self.rationale = rationale
+    }
+}
+
+extension GuardianApprovalReview: Codable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(status, forKey: .status)
+        try container.encodeNilOrValue(riskLevel, forKey: .riskLevel)
+        try container.encodeNilOrValue(userAuthorization, forKey: .userAuthorization)
+        try container.encodeNilOrValue(rationale, forKey: .rationale)
+    }
+}
+
+public enum GuardianApprovalReviewCommandSource: String, Codable, Equatable, Sendable {
+    case shell
+    case unifiedExec
+}
+
+public enum GuardianApprovalReviewAction: Equatable, Sendable {
+    case command(source: GuardianApprovalReviewCommandSource, command: String, cwd: String)
+    case execve(source: GuardianApprovalReviewCommandSource, program: String, argv: [String], cwd: String)
+    case applyPatch(cwd: String, files: [String])
+    case networkAccess(target: String, host: String, protocol: NetworkApprovalProtocol, port: UInt16)
+    case mcpToolCall(
+        server: String,
+        toolName: String,
+        connectorID: String?,
+        connectorName: String?,
+        toolTitle: String?
+    )
+    case requestPermissions(reason: String?, permissions: RequestPermissionProfile)
+
+    private enum CodingKeys: String, CodingKey {
+        case type
+        case source
+        case command
+        case cwd
+        case program
+        case argv
+        case files
+        case target
+        case host
+        case `protocol`
+        case port
+        case server
+        case toolName
+        case connectorID = "connectorId"
+        case connectorName
+        case toolTitle
+        case reason
+        case permissions
+    }
+
+    private enum ActionType: String, Codable {
+        case command
+        case execve
+        case applyPatch
+        case networkAccess
+        case mcpToolCall
+        case requestPermissions
+    }
+}
+
+extension GuardianApprovalReviewAction: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        switch try container.decode(ActionType.self, forKey: .type) {
+        case .command:
+            self = .command(
+                source: try container.decode(GuardianApprovalReviewCommandSource.self, forKey: .source),
+                command: try container.decode(String.self, forKey: .command),
+                cwd: try container.decode(String.self, forKey: .cwd)
+            )
+        case .execve:
+            self = .execve(
+                source: try container.decode(GuardianApprovalReviewCommandSource.self, forKey: .source),
+                program: try container.decode(String.self, forKey: .program),
+                argv: try container.decode([String].self, forKey: .argv),
+                cwd: try container.decode(String.self, forKey: .cwd)
+            )
+        case .applyPatch:
+            self = .applyPatch(
+                cwd: try container.decode(String.self, forKey: .cwd),
+                files: try container.decode([String].self, forKey: .files)
+            )
+        case .networkAccess:
+            self = .networkAccess(
+                target: try container.decode(String.self, forKey: .target),
+                host: try container.decode(String.self, forKey: .host),
+                protocol: try Self.decodeAppServerNetworkApprovalProtocol(container, forKey: .protocol),
+                port: try container.decode(UInt16.self, forKey: .port)
+            )
+        case .mcpToolCall:
+            self = .mcpToolCall(
+                server: try container.decode(String.self, forKey: .server),
+                toolName: try container.decode(String.self, forKey: .toolName),
+                connectorID: try container.decodeIfPresent(String.self, forKey: .connectorID),
+                connectorName: try container.decodeIfPresent(String.self, forKey: .connectorName),
+                toolTitle: try container.decodeIfPresent(String.self, forKey: .toolTitle)
+            )
+        case .requestPermissions:
+            self = .requestPermissions(
+                reason: try container.decodeIfPresent(String.self, forKey: .reason),
+                permissions: try container.decode(RequestPermissionProfile.self, forKey: .permissions)
+            )
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case let .command(source, command, cwd):
+            try container.encode(ActionType.command, forKey: .type)
+            try container.encode(source, forKey: .source)
+            try container.encode(command, forKey: .command)
+            try container.encode(cwd, forKey: .cwd)
+        case let .execve(source, program, argv, cwd):
+            try container.encode(ActionType.execve, forKey: .type)
+            try container.encode(source, forKey: .source)
+            try container.encode(program, forKey: .program)
+            try container.encode(argv, forKey: .argv)
+            try container.encode(cwd, forKey: .cwd)
+        case let .applyPatch(cwd, files):
+            try container.encode(ActionType.applyPatch, forKey: .type)
+            try container.encode(cwd, forKey: .cwd)
+            try container.encode(files, forKey: .files)
+        case let .networkAccess(target, host, protocolValue, port):
+            try container.encode(ActionType.networkAccess, forKey: .type)
+            try container.encode(target, forKey: .target)
+            try container.encode(host, forKey: .host)
+            try container.encode(Self.appServerNetworkApprovalProtocolName(protocolValue), forKey: .protocol)
+            try container.encode(port, forKey: .port)
+        case let .mcpToolCall(server, toolName, connectorID, connectorName, toolTitle):
+            try container.encode(ActionType.mcpToolCall, forKey: .type)
+            try container.encode(server, forKey: .server)
+            try container.encode(toolName, forKey: .toolName)
+            try container.encodeNilOrValue(connectorID, forKey: .connectorID)
+            try container.encodeNilOrValue(connectorName, forKey: .connectorName)
+            try container.encodeNilOrValue(toolTitle, forKey: .toolTitle)
+        case let .requestPermissions(reason, permissions):
+            try container.encode(ActionType.requestPermissions, forKey: .type)
+            try container.encodeNilOrValue(reason, forKey: .reason)
+            try container.encode(permissions, forKey: .permissions)
+        }
+    }
+
+    private static func decodeAppServerNetworkApprovalProtocol<Key: CodingKey>(
+        _ container: KeyedDecodingContainer<Key>,
+        forKey key: Key
+    ) throws -> NetworkApprovalProtocol {
+        switch try container.decode(String.self, forKey: key) {
+        case "http":
+            return .http
+        case "https":
+            return .https
+        case "socks5Tcp", "socks5_tcp":
+            return .socks5Tcp
+        case "socks5Udp", "socks5_udp":
+            return .socks5Udp
+        case let value:
+            throw DecodingError.dataCorruptedError(
+                forKey: key,
+                in: container,
+                debugDescription: "Unsupported network approval protocol: \(value)"
+            )
+        }
+    }
+
+    private static func appServerNetworkApprovalProtocolName(_ value: NetworkApprovalProtocol) -> String {
+        switch value {
+        case .http:
+            return "http"
+        case .https:
+            return "https"
+        case .socks5Tcp:
+            return "socks5Tcp"
+        case .socks5Udp:
+            return "socks5Udp"
+        }
+    }
+}
+
+public enum AutoReviewDecisionSource: String, Codable, Equatable, Sendable {
+    case agent
+}
+
+public struct ItemGuardianApprovalReviewStartedNotification: Equatable, Sendable {
+    public let threadID: String
+    public let turnID: String
+    public let startedAtMilliseconds: Int64
+    public let reviewID: String
+    public let targetItemID: String?
+    public let review: GuardianApprovalReview
+    public let action: GuardianApprovalReviewAction
+
+    private enum CodingKeys: String, CodingKey {
+        case threadID = "threadId"
+        case turnID = "turnId"
+        case startedAtMilliseconds = "startedAtMs"
+        case reviewID = "reviewId"
+        case targetItemID = "targetItemId"
+        case review
+        case action
+    }
+
+    public init(
+        threadID: String,
+        turnID: String,
+        startedAtMilliseconds: Int64,
+        reviewID: String,
+        targetItemID: String?,
+        review: GuardianApprovalReview,
+        action: GuardianApprovalReviewAction
+    ) {
+        self.threadID = threadID
+        self.turnID = turnID
+        self.startedAtMilliseconds = startedAtMilliseconds
+        self.reviewID = reviewID
+        self.targetItemID = targetItemID
+        self.review = review
+        self.action = action
+    }
+}
+
+extension ItemGuardianApprovalReviewStartedNotification: Codable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(threadID, forKey: .threadID)
+        try container.encode(turnID, forKey: .turnID)
+        try container.encode(startedAtMilliseconds, forKey: .startedAtMilliseconds)
+        try container.encode(reviewID, forKey: .reviewID)
+        try container.encodeNilOrValue(targetItemID, forKey: .targetItemID)
+        try container.encode(review, forKey: .review)
+        try container.encode(action, forKey: .action)
+    }
+}
+
+public struct ItemGuardianApprovalReviewCompletedNotification: Equatable, Sendable {
+    public let threadID: String
+    public let turnID: String
+    public let startedAtMilliseconds: Int64
+    public let completedAtMilliseconds: Int64
+    public let reviewID: String
+    public let targetItemID: String?
+    public let decisionSource: AutoReviewDecisionSource
+    public let review: GuardianApprovalReview
+    public let action: GuardianApprovalReviewAction
+
+    private enum CodingKeys: String, CodingKey {
+        case threadID = "threadId"
+        case turnID = "turnId"
+        case startedAtMilliseconds = "startedAtMs"
+        case completedAtMilliseconds = "completedAtMs"
+        case reviewID = "reviewId"
+        case targetItemID = "targetItemId"
+        case decisionSource
+        case review
+        case action
+    }
+
+    public init(
+        threadID: String,
+        turnID: String,
+        startedAtMilliseconds: Int64,
+        completedAtMilliseconds: Int64,
+        reviewID: String,
+        targetItemID: String?,
+        decisionSource: AutoReviewDecisionSource,
+        review: GuardianApprovalReview,
+        action: GuardianApprovalReviewAction
+    ) {
+        self.threadID = threadID
+        self.turnID = turnID
+        self.startedAtMilliseconds = startedAtMilliseconds
+        self.completedAtMilliseconds = completedAtMilliseconds
+        self.reviewID = reviewID
+        self.targetItemID = targetItemID
+        self.decisionSource = decisionSource
+        self.review = review
+        self.action = action
+    }
+}
+
+extension ItemGuardianApprovalReviewCompletedNotification: Codable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(threadID, forKey: .threadID)
+        try container.encode(turnID, forKey: .turnID)
+        try container.encode(startedAtMilliseconds, forKey: .startedAtMilliseconds)
+        try container.encode(completedAtMilliseconds, forKey: .completedAtMilliseconds)
+        try container.encode(reviewID, forKey: .reviewID)
+        try container.encodeNilOrValue(targetItemID, forKey: .targetItemID)
+        try container.encode(decisionSource, forKey: .decisionSource)
+        try container.encode(review, forKey: .review)
+        try container.encode(action, forKey: .action)
+    }
+}
+
 public struct ErrorNotification: Equatable, Codable, Sendable {
     public let error: AppServerTurnError
     public let willRetry: Bool
--- Tests/CodexCoreTests/AppServerNotificationProtocolTests.swift
+++ Tests/CodexCoreTests/AppServerNotificationProtocolTests.swift
@@ -77,6 +77,164 @@ final class AppServerNotificationProtocolTests: XCTestCase {
         )
     }
 
+    func testGuardianApprovalReviewNotificationsEncodeRustV2WireShape() throws {
+        let started = ItemGuardianApprovalReviewStartedNotification(
+            threadID: "thread-1",
+            turnID: "turn-1",
+            startedAtMilliseconds: 1_000,
+            reviewID: "review-1",
+            targetItemID: nil,
+            review: GuardianApprovalReview(status: .inProgress),
+            action: .command(source: .unifiedExec, command: "git status", cwd: "/repo")
+        )
+
+        try XCTAssertJSONObjectEqual(started, [
+            "threadId": "thread-1",
+            "turnId": "turn-1",
+            "startedAtMs": 1_000,
+            "reviewId": "review-1",
+            "targetItemId": NSNull(),
+            "review": [
+                "status": "inProgress",
+                "riskLevel": NSNull(),
+                "userAuthorization": NSNull(),
+                "rationale": NSNull()
+            ],
+            "action": [
+                "type": "command",
+                "source": "unifiedExec",
+                "command": "git status",
+                "cwd": "/repo"
+            ]
+        ])
+
+        let decodedStarted = try JSONDecoder().decode(
+            ItemGuardianApprovalReviewStartedNotification.self,
+            from: Data("""
+            {
+              "threadId": "thread-1",
+              "turnId": "turn-1",
+              "startedAtMs": 1000,
+              "reviewId": "review-1",
+              "targetItemId": null,
+              "review": {
+                "status": "inProgress",
+                "riskLevel": null,
+                "userAuthorization": null,
+                "rationale": null
+              },
+              "action": {
+                "type": "command",
+                "source": "unifiedExec",
+                "command": "git status",
+                "cwd": "/repo"
+              }
+            }
+            """.utf8)
+        )
+        XCTAssertEqual(decodedStarted, started)
+
+        let completed = ItemGuardianApprovalReviewCompletedNotification(
+            threadID: "thread-1",
+            turnID: "turn-1",
+            startedAtMilliseconds: 1_000,
+            completedAtMilliseconds: 1_042,
+            reviewID: "review-2",
+            targetItemID: "item-2",
+            decisionSource: .agent,
+            review: GuardianApprovalReview(
+                status: .denied,
+                riskLevel: .high,
+                userAuthorization: .low,
+                rationale: "too risky"
+            ),
+            action: .mcpToolCall(
+                server: "github",
+                toolName: "delete_repo",
+                connectorID: nil,
+                connectorName: "GitHub",
+                toolTitle: nil
+            )
+        )
+
+        try XCTAssertJSONObjectEqual(completed, [
+            "threadId": "thread-1",
+            "turnId": "turn-1",
+            "startedAtMs": 1_000,
+            "completedAtMs": 1_042,
+            "reviewId": "review-2",
+            "targetItemId": "item-2",
+            "decisionSource": "agent",
+            "review": [
+                "status": "denied",
+                "riskLevel": "high",
+                "userAuthorization": "low",
+                "rationale": "too risky"
+            ],
+            "action": [
+                "type": "mcpToolCall",
+                "server": "github",
+                "toolName": "delete_repo",
+                "connectorId": NSNull(),
+                "connectorName": "GitHub",
+                "toolTitle": NSNull()
+            ]
+        ])
+
+        let decodedCompleted = try JSONDecoder().decode(
+            ItemGuardianApprovalReviewCompletedNotification.self,
+            from: Data("""
+            {
+              "threadId": "thread-1",
+              "turnId": "turn-1",
+              "startedAtMs": 1000,
+              "completedAtMs": 1042,
+              "reviewId": "review-2",
+              "targetItemId": "item-2",
+              "decisionSource": "agent",
+              "review": {
+                "status": "denied",
+                "riskLevel": "high",
+                "userAuthorization": "low",
+                "rationale": "too risky"
+              },
+              "action": {
+                "type": "mcpToolCall",
+                "server": "github",
+                "toolName": "delete_repo",
+                "connectorId": null,
+                "connectorName": "GitHub",
+                "toolTitle": null
+              }
+            }
+            """.utf8)
+        )
+        XCTAssertEqual(decodedCompleted, completed)
+
+        try XCTAssertJSONObjectEqual(
+            GuardianApprovalReviewAction.networkAccess(
+                target: "https://example.com",
+                host: "example.com",
+                protocol: .socks5Tcp,
+                port: 443
+            ),
+            [
+                "type": "networkAccess",
+                "target": "https://example.com",
+                "host": "example.com",
+                "protocol": "socks5Tcp",
+                "port": 443
+            ]
+        )
+        XCTAssertEqual(
+            try JSONDecoder().decode(
+                GuardianApprovalReviewAction.self,
+                from: Data(#"{"type":"networkAccess","target":"https://example.com","host":"example.com","protocol":"socks5Tcp","port":443}"#.utf8)
+            ),
+            .networkAccess(target: "https://example.com", host: "example.com", protocol: .socks5Tcp, port: 443)
+        )
+    }
+
     func testRemoteControlStatusChangedNotificationUsesRustCamelCaseShape() throws {
         try XCTAssertJSONObjectEqual(
             RemoteControlStatusChangedNotification(
