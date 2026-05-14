52ab2884c710f30558d99067241adee5df6ceacf
mweinbach <maxweinbach5@gmail.com>
2026-05-14 01:57:06 -0400

    Add account login protocol models

--- Sources/CodexCore/AppServerAccountProtocol.swift
+++ Sources/CodexCore/AppServerAccountProtocol.swift
@@ -47,6 +47,235 @@ extension Account: Codable {
     }
 }
 
+public enum LoginAccountParams: Equatable, Sendable {
+    case apiKey(apiKey: String)
+    case chatGPT(codexStreamlinedLogin: Bool)
+    case chatGPTDeviceCode
+    case chatGPTAuthTokens(accessToken: String, chatGPTAccountID: String, chatGPTPlanType: String?)
+
+    private enum CodingKeys: String, CodingKey {
+        case type
+        case apiKey
+        case codexStreamlinedLogin
+        case accessToken
+        case chatGPTAccountID = "chatgptAccountId"
+        case chatGPTPlanType = "chatgptPlanType"
+    }
+
+    private enum LoginType: String, Codable {
+        case apiKey
+        case chatGPT = "chatgpt"
+        case chatGPTDeviceCode = "chatgptDeviceCode"
+        case chatGPTAuthTokens = "chatgptAuthTokens"
+    }
+}
+
+extension LoginAccountParams: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        switch try container.decode(LoginType.self, forKey: .type) {
+        case .apiKey:
+            self = .apiKey(apiKey: try container.decode(String.self, forKey: .apiKey))
+        case .chatGPT:
+            self = .chatGPT(
+                codexStreamlinedLogin: try container.decodeIfPresent(
+                    Bool.self,
+                    forKey: .codexStreamlinedLogin
+                ) ?? false
+            )
+        case .chatGPTDeviceCode:
+            self = .chatGPTDeviceCode
+        case .chatGPTAuthTokens:
+            self = .chatGPTAuthTokens(
+                accessToken: try container.decode(String.self, forKey: .accessToken),
+                chatGPTAccountID: try container.decode(String.self, forKey: .chatGPTAccountID),
+                chatGPTPlanType: try container.decodeIfPresent(String.self, forKey: .chatGPTPlanType)
+            )
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case let .apiKey(apiKey):
+            try container.encode(LoginType.apiKey, forKey: .type)
+            try container.encode(apiKey, forKey: .apiKey)
+        case let .chatGPT(codexStreamlinedLogin):
+            try container.encode(LoginType.chatGPT, forKey: .type)
+            if codexStreamlinedLogin {
+                try container.encode(codexStreamlinedLogin, forKey: .codexStreamlinedLogin)
+            }
+        case .chatGPTDeviceCode:
+            try container.encode(LoginType.chatGPTDeviceCode, forKey: .type)
+        case let .chatGPTAuthTokens(accessToken, chatGPTAccountID, chatGPTPlanType):
+            try container.encode(LoginType.chatGPTAuthTokens, forKey: .type)
+            try container.encode(accessToken, forKey: .accessToken)
+            try container.encode(chatGPTAccountID, forKey: .chatGPTAccountID)
+            try container.encodeNilOrValue(chatGPTPlanType, forKey: .chatGPTPlanType)
+        }
+    }
+}
+
+public enum LoginAccountResponse: Equatable, Sendable {
+    case apiKey
+    case chatGPT(loginID: String, authURL: String)
+    case chatGPTDeviceCode(loginID: String, verificationURL: String, userCode: String)
+    case chatGPTAuthTokens
+
+    private enum CodingKeys: String, CodingKey {
+        case type
+        case loginID = "loginId"
+        case authURL = "authUrl"
+        case verificationURL = "verificationUrl"
+        case userCode
+    }
+
+    private enum LoginType: String, Codable {
+        case apiKey
+        case chatGPT = "chatgpt"
+        case chatGPTDeviceCode = "chatgptDeviceCode"
+        case chatGPTAuthTokens = "chatgptAuthTokens"
+    }
+}
+
+extension LoginAccountResponse: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        switch try container.decode(LoginType.self, forKey: .type) {
+        case .apiKey:
+            self = .apiKey
+        case .chatGPT:
+            self = .chatGPT(
+                loginID: try container.decode(String.self, forKey: .loginID),
+                authURL: try container.decode(String.self, forKey: .authURL)
+            )
+        case .chatGPTDeviceCode:
+            self = .chatGPTDeviceCode(
+                loginID: try container.decode(String.self, forKey: .loginID),
+                verificationURL: try container.decode(String.self, forKey: .verificationURL),
+                userCode: try container.decode(String.self, forKey: .userCode)
+            )
+        case .chatGPTAuthTokens:
+            self = .chatGPTAuthTokens
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .apiKey:
+            try container.encode(LoginType.apiKey, forKey: .type)
+        case let .chatGPT(loginID, authURL):
+            try container.encode(LoginType.chatGPT, forKey: .type)
+            try container.encode(loginID, forKey: .loginID)
+            try container.encode(authURL, forKey: .authURL)
+        case let .chatGPTDeviceCode(loginID, verificationURL, userCode):
+            try container.encode(LoginType.chatGPTDeviceCode, forKey: .type)
+            try container.encode(loginID, forKey: .loginID)
+            try container.encode(verificationURL, forKey: .verificationURL)
+            try container.encode(userCode, forKey: .userCode)
+        case .chatGPTAuthTokens:
+            try container.encode(LoginType.chatGPTAuthTokens, forKey: .type)
+        }
+    }
+}
+
+public struct CancelLoginAccountParams: Equatable, Codable, Sendable {
+    public let loginID: String
+
+    private enum CodingKeys: String, CodingKey {
+        case loginID = "loginId"
+    }
+
+    public init(loginID: String) {
+        self.loginID = loginID
+    }
+}
+
+public enum CancelLoginAccountStatus: String, Codable, Equatable, Sendable {
+    case canceled
+    case notFound
+}
+
+public struct CancelLoginAccountResponse: Equatable, Codable, Sendable {
+    public let status: CancelLoginAccountStatus
+
+    public init(status: CancelLoginAccountStatus) {
+        self.status = status
+    }
+}
+
+public struct LogoutAccountResponse: Equatable, Codable, Sendable {
+    public init() {}
+}
+
+public enum ChatGPTAuthTokensRefreshReason: String, Codable, Equatable, Sendable {
+    case unauthorized
+}
+
+public struct ChatGPTAuthTokensRefreshParams: Equatable, Sendable {
+    public let reason: ChatGPTAuthTokensRefreshReason
+    public let previousAccountID: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case reason
+        case previousAccountID = "previousAccountId"
+    }
+
+    public init(reason: ChatGPTAuthTokensRefreshReason, previousAccountID: String?) {
+        self.reason = reason
+        self.previousAccountID = previousAccountID
+    }
+}
+
+extension ChatGPTAuthTokensRefreshParams: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        reason = try container.decode(ChatGPTAuthTokensRefreshReason.self, forKey: .reason)
+        previousAccountID = try container.decodeIfPresent(String.self, forKey: .previousAccountID)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(reason, forKey: .reason)
+        try container.encodeNilOrValue(previousAccountID, forKey: .previousAccountID)
+    }
+}
+
+public struct ChatGPTAuthTokensRefreshResponse: Equatable, Sendable {
+    public let accessToken: String
+    public let chatGPTAccountID: String
+    public let chatGPTPlanType: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case accessToken
+        case chatGPTAccountID = "chatgptAccountId"
+        case chatGPTPlanType = "chatgptPlanType"
+    }
+
+    public init(accessToken: String, chatGPTAccountID: String, chatGPTPlanType: String?) {
+        self.accessToken = accessToken
+        self.chatGPTAccountID = chatGPTAccountID
+        self.chatGPTPlanType = chatGPTPlanType
+    }
+}
+
+extension ChatGPTAuthTokensRefreshResponse: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        accessToken = try container.decode(String.self, forKey: .accessToken)
+        chatGPTAccountID = try container.decode(String.self, forKey: .chatGPTAccountID)
+        chatGPTPlanType = try container.decodeIfPresent(String.self, forKey: .chatGPTPlanType)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(accessToken, forKey: .accessToken)
+        try container.encode(chatGPTAccountID, forKey: .chatGPTAccountID)
+        try container.encodeNilOrValue(chatGPTPlanType, forKey: .chatGPTPlanType)
+    }
+}
+
 public struct GetAccountParams: Equatable, Sendable {
     public let refreshToken: Bool
 
--- Tests/CodexCoreTests/AppServerAccountProtocolTests.swift
+++ Tests/CodexCoreTests/AppServerAccountProtocolTests.swift
@@ -15,6 +15,146 @@ final class AppServerAccountProtocolTests: XCTestCase {
         )
     }
 
+    func testLoginAccountParamsEncodeRustTaggedShape() throws {
+        try XCTAssertJSONObjectEqual(
+            LoginAccountParams.apiKey(apiKey: "sk-test"),
+            [
+                "type": "apiKey",
+                "apiKey": "sk-test"
+            ]
+        )
+        try XCTAssertJSONObjectEqual(
+            LoginAccountParams.chatGPT(codexStreamlinedLogin: false),
+            [
+                "type": "chatgpt"
+            ]
+        )
+        try XCTAssertJSONObjectEqual(
+            LoginAccountParams.chatGPT(codexStreamlinedLogin: true),
+            [
+                "type": "chatgpt",
+                "codexStreamlinedLogin": true
+            ]
+        )
+        try XCTAssertJSONObjectEqual(
+            LoginAccountParams.chatGPTDeviceCode,
+            [
+                "type": "chatgptDeviceCode"
+            ]
+        )
+        try XCTAssertJSONObjectEqual(
+            LoginAccountParams.chatGPTAuthTokens(
+                accessToken: "access-token",
+                chatGPTAccountID: "org-1",
+                chatGPTPlanType: nil
+            ),
+            [
+                "type": "chatgptAuthTokens",
+                "accessToken": "access-token",
+                "chatgptAccountId": "org-1",
+                "chatgptPlanType": NSNull()
+            ]
+        )
+    }
+
+    func testLoginAccountResponseEncodeRustTaggedShape() throws {
+        try XCTAssertJSONObjectEqual(LoginAccountResponse.apiKey, ["type": "apiKey"])
+        try XCTAssertJSONObjectEqual(
+            LoginAccountResponse.chatGPT(loginID: "login-1", authURL: "https://example.test/auth"),
+            [
+                "type": "chatgpt",
+                "loginId": "login-1",
+                "authUrl": "https://example.test/auth"
+            ]
+        )
+        try XCTAssertJSONObjectEqual(
+            LoginAccountResponse.chatGPTDeviceCode(
+                loginID: "login-2",
+                verificationURL: "https://example.test/device",
+                userCode: "ABCD-EFGH"
+            ),
+            [
+                "type": "chatgptDeviceCode",
+                "loginId": "login-2",
+                "verificationUrl": "https://example.test/device",
+                "userCode": "ABCD-EFGH"
+            ]
+        )
+        try XCTAssertJSONObjectEqual(LoginAccountResponse.chatGPTAuthTokens, ["type": "chatgptAuthTokens"])
+    }
+
+    func testLoginSupportPayloadsEncodeRustWireShapes() throws {
+        try XCTAssertJSONObjectEqual(
+            CancelLoginAccountParams(loginID: "login-1"),
+            [
+                "loginId": "login-1"
+            ]
+        )
+        try XCTAssertJSONObjectEqual(
+            CancelLoginAccountResponse(status: .notFound),
+            [
+                "status": "notFound"
+            ]
+        )
+        try XCTAssertJSONObjectEqual(LogoutAccountResponse(), [:])
+    }
+
+    func testExternalAuthTokenRefreshPayloadsEncodeExplicitNullOptionalsLikeRustProtocol() throws {
+        try XCTAssertJSONObjectEqual(
+            ChatGPTAuthTokensRefreshParams(reason: .unauthorized, previousAccountID: nil),
+            [
+                "reason": "unauthorized",
+                "previousAccountId": NSNull()
+            ]
+        )
+        try XCTAssertJSONObjectEqual(
+            ChatGPTAuthTokensRefreshResponse(
+                accessToken: "replacement-token",
+                chatGPTAccountID: "org-1",
+                chatGPTPlanType: nil
+            ),
+            [
+                "accessToken": "replacement-token",
+                "chatgptAccountId": "org-1",
+                "chatgptPlanType": NSNull()
+            ]
+        )
+    }
+
+    func testLoginAccountDecodeMatchesRustProtocol() throws {
+        let missingStreamlinedFlag = try JSONDecoder().decode(
+            LoginAccountParams.self,
+            from: Data(#"{"type":"chatgpt"}"#.utf8)
+        )
+        XCTAssertEqual(missingStreamlinedFlag, .chatGPT(codexStreamlinedLogin: false))
+
+        let authTokens = try JSONDecoder().decode(
+            LoginAccountParams.self,
+            from: Data(
+                #"{"type":"chatgptAuthTokens","accessToken":"access-token","chatgptAccountId":"org-1","chatgptPlanType":null}"#.utf8
+            )
+        )
+        XCTAssertEqual(
+            authTokens,
+            .chatGPTAuthTokens(accessToken: "access-token", chatGPTAccountID: "org-1", chatGPTPlanType: nil)
+        )
+
+        let deviceCode = try JSONDecoder().decode(
+            LoginAccountResponse.self,
+            from: Data(
+                #"{"type":"chatgptDeviceCode","loginId":"login-2","verificationUrl":"https://example.test/device","userCode":"ABCD-EFGH"}"#.utf8
+            )
+        )
+        XCTAssertEqual(
+            deviceCode,
+            .chatGPTDeviceCode(
+                loginID: "login-2",
+                verificationURL: "https://example.test/device",
+                userCode: "ABCD-EFGH"
+            )
+        )
+    }
+
     func testGetAccountParamsDefaultAndEncodingMatchRustProtocol() throws {
         let decoded = try JSONDecoder().decode(GetAccountParams.self, from: Data(#"{}"#.utf8))
         XCTAssertEqual(decoded, GetAccountParams(refreshToken: false))
