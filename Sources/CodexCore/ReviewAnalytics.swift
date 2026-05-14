1f710421521d39c101effe863c7b62544be9ee52
mweinbach <maxweinbach5@gmail.com>
2026-05-14 09:05:31 -0400

    Add command tool item analytics schema

--- Sources/CodexCore/ReviewAnalytics.swift
+++ Sources/CodexCore/ReviewAnalytics.swift
@@ -105,6 +105,261 @@ public enum ReviewResolution: String, Codable, Equatable, Sendable {
     case networkPolicyAmendment = "network_policy_amendment"
 }
 
+public enum ToolItemFinalApprovalOutcome: String, Codable, Equatable, Sendable {
+    case unknown
+    case notNeeded = "not_needed"
+    case configAllowed = "config_allowed"
+    case policyForbidden = "policy_forbidden"
+    case guardianApproved = "guardian_approved"
+    case guardianDenied = "guardian_denied"
+    case guardianAborted = "guardian_aborted"
+    case userApproved = "user_approved"
+    case userApprovedForSession = "user_approved_for_session"
+    case userDenied = "user_denied"
+    case userAborted = "user_aborted"
+}
+
+public enum ToolItemTerminalStatus: String, Codable, Equatable, Sendable {
+    case completed
+    case failed
+    case rejected
+    case interrupted
+}
+
+public enum ToolItemFailureKind: String, Codable, Equatable, Sendable {
+    case toolError = "tool_error"
+    case approvalDenied = "approval_denied"
+    case approvalAborted = "approval_aborted"
+    case sandboxDenied = "sandbox_denied"
+    case policyForbidden = "policy_forbidden"
+}
+
+public enum CommandExecutionSource: String, Codable, Equatable, Sendable {
+    case agent
+    case userShell = "user_shell"
+    case unifiedExecStartup = "unified_exec_startup"
+    case unifiedExecInteraction = "unified_exec_interaction"
+}
+
+public struct CodexToolItemEventBase: Equatable, Encodable, Sendable {
+    public let threadID: String
+    public let turnID: String
+    public let itemID: String
+    public let appServerClient: CodexAppServerClientMetadata
+    public let runtime: CodexRuntimeMetadata
+    public let threadSource: ThreadSource?
+    public let subagentSource: String?
+    public let parentThreadID: String?
+    public let toolName: String
+    public let startedAtMilliseconds: UInt64
+    public let completedAtMilliseconds: UInt64
+    public let durationMilliseconds: UInt64?
+    public let executionDurationMilliseconds: UInt64?
+    public let reviewCount: UInt64
+    public let guardianReviewCount: UInt64
+    public let userReviewCount: UInt64
+    public let finalApprovalOutcome: ToolItemFinalApprovalOutcome
+    public let terminalStatus: ToolItemTerminalStatus
+    public let failureKind: ToolItemFailureKind?
+    public let requestedAdditionalPermissions: Bool
+    public let requestedNetworkAccess: Bool
+
+    public init(
+        threadID: String,
+        turnID: String,
+        itemID: String,
+        appServerClient: CodexAppServerClientMetadata,
+        runtime: CodexRuntimeMetadata,
+        threadSource: ThreadSource? = nil,
+        subagentSource: String? = nil,
+        parentThreadID: String? = nil,
+        toolName: String,
+        startedAtMilliseconds: UInt64,
+        completedAtMilliseconds: UInt64,
+        durationMilliseconds: UInt64? = nil,
+        executionDurationMilliseconds: UInt64? = nil,
+        reviewCount: UInt64,
+        guardianReviewCount: UInt64,
+        userReviewCount: UInt64,
+        finalApprovalOutcome: ToolItemFinalApprovalOutcome,
+        terminalStatus: ToolItemTerminalStatus,
+        failureKind: ToolItemFailureKind? = nil,
+        requestedAdditionalPermissions: Bool,
+        requestedNetworkAccess: Bool
+    ) {
+        self.threadID = threadID
+        self.turnID = turnID
+        self.itemID = itemID
+        self.appServerClient = appServerClient
+        self.runtime = runtime
+        self.threadSource = threadSource
+        self.subagentSource = subagentSource
+        self.parentThreadID = parentThreadID
+        self.toolName = toolName
+        self.startedAtMilliseconds = startedAtMilliseconds
+        self.completedAtMilliseconds = completedAtMilliseconds
+        self.durationMilliseconds = durationMilliseconds
+        self.executionDurationMilliseconds = executionDurationMilliseconds
+        self.reviewCount = reviewCount
+        self.guardianReviewCount = guardianReviewCount
+        self.userReviewCount = userReviewCount
+        self.finalApprovalOutcome = finalApprovalOutcome
+        self.terminalStatus = terminalStatus
+        self.failureKind = failureKind
+        self.requestedAdditionalPermissions = requestedAdditionalPermissions
+        self.requestedNetworkAccess = requestedNetworkAccess
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case threadID = "thread_id"
+        case turnID = "turn_id"
+        case itemID = "item_id"
+        case appServerClient = "app_server_client"
+        case runtime
+        case threadSource = "thread_source"
+        case subagentSource = "subagent_source"
+        case parentThreadID = "parent_thread_id"
+        case toolName = "tool_name"
+        case startedAtMilliseconds = "started_at_ms"
+        case completedAtMilliseconds = "completed_at_ms"
+        case durationMilliseconds = "duration_ms"
+        case executionDurationMilliseconds = "execution_duration_ms"
+        case reviewCount = "review_count"
+        case guardianReviewCount = "guardian_review_count"
+        case userReviewCount = "user_review_count"
+        case finalApprovalOutcome = "final_approval_outcome"
+        case terminalStatus = "terminal_status"
+        case failureKind = "failure_kind"
+        case requestedAdditionalPermissions = "requested_additional_permissions"
+        case requestedNetworkAccess = "requested_network_access"
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(threadID, forKey: .threadID)
+        try container.encode(turnID, forKey: .turnID)
+        try container.encode(itemID, forKey: .itemID)
+        try container.encode(appServerClient, forKey: .appServerClient)
+        try container.encode(runtime, forKey: .runtime)
+        try container.encodeNilOrValue(threadSource, forKey: .threadSource)
+        try container.encodeNilOrValue(subagentSource, forKey: .subagentSource)
+        try container.encodeNilOrValue(parentThreadID, forKey: .parentThreadID)
+        try container.encode(toolName, forKey: .toolName)
+        try container.encode(startedAtMilliseconds, forKey: .startedAtMilliseconds)
+        try container.encode(completedAtMilliseconds, forKey: .completedAtMilliseconds)
+        try container.encodeNilOrValue(durationMilliseconds, forKey: .durationMilliseconds)
+        try container.encodeNilOrValue(executionDurationMilliseconds, forKey: .executionDurationMilliseconds)
+        try container.encode(reviewCount, forKey: .reviewCount)
+        try container.encode(guardianReviewCount, forKey: .guardianReviewCount)
+        try container.encode(userReviewCount, forKey: .userReviewCount)
+        try container.encode(finalApprovalOutcome, forKey: .finalApprovalOutcome)
+        try container.encode(terminalStatus, forKey: .terminalStatus)
+        try container.encodeNilOrValue(failureKind, forKey: .failureKind)
+        try container.encode(requestedAdditionalPermissions, forKey: .requestedAdditionalPermissions)
+        try container.encode(requestedNetworkAccess, forKey: .requestedNetworkAccess)
+    }
+}
+
+public struct CodexCommandActionCounts: Equatable, Sendable {
+    public let total: UInt64
+    public let read: UInt64
+    public let listFiles: UInt64
+    public let search: UInt64
+    public let unknown: UInt64
+
+    public init(total: UInt64, read: UInt64, listFiles: UInt64, search: UInt64, unknown: UInt64) {
+        self.total = total
+        self.read = read
+        self.listFiles = listFiles
+        self.search = search
+        self.unknown = unknown
+    }
+
+    public init(actions: [AppServerProtocol.CommandAction]) {
+        var read: UInt64 = 0
+        var listFiles: UInt64 = 0
+        var search: UInt64 = 0
+        var unknown: UInt64 = 0
+
+        for action in actions {
+            switch action {
+            case .read:
+                read += 1
+            case .listFiles:
+                listFiles += 1
+            case .search:
+                search += 1
+            case .unknown:
+                unknown += 1
+            }
+        }
+
+        self.init(
+            total: UInt64(actions.count),
+            read: read,
+            listFiles: listFiles,
+            search: search,
+            unknown: unknown
+        )
+    }
+}
+
+public struct CodexCommandExecutionEventParams: Equatable, Encodable, Sendable {
+    public let base: CodexToolItemEventBase
+    public let commandExecutionSource: CommandExecutionSource
+    public let exitCode: Int32?
+    public let commandActionCounts: CodexCommandActionCounts
+
+    public init(
+        base: CodexToolItemEventBase,
+        commandExecutionSource: CommandExecutionSource,
+        exitCode: Int32? = nil,
+        commandActionCounts: CodexCommandActionCounts
+    ) {
+        self.base = base
+        self.commandExecutionSource = commandExecutionSource
+        self.exitCode = exitCode
+        self.commandActionCounts = commandActionCounts
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case commandExecutionSource = "command_execution_source"
+        case exitCode = "exit_code"
+        case commandTotalActionCount = "command_total_action_count"
+        case commandReadActionCount = "command_read_action_count"
+        case commandListFilesActionCount = "command_list_files_action_count"
+        case commandSearchActionCount = "command_search_action_count"
+        case commandUnknownActionCount = "command_unknown_action_count"
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        try base.encode(to: encoder)
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(commandExecutionSource, forKey: .commandExecutionSource)
+        try container.encodeNilOrValue(exitCode, forKey: .exitCode)
+        try container.encode(commandActionCounts.total, forKey: .commandTotalActionCount)
+        try container.encode(commandActionCounts.read, forKey: .commandReadActionCount)
+        try container.encode(commandActionCounts.listFiles, forKey: .commandListFilesActionCount)
+        try container.encode(commandActionCounts.search, forKey: .commandSearchActionCount)
+        try container.encode(commandActionCounts.unknown, forKey: .commandUnknownActionCount)
+    }
+}
+
+public struct CodexCommandExecutionEventRequest: Equatable, Encodable, Sendable {
+    public let eventType: String
+    public let eventParams: CodexCommandExecutionEventParams
+
+    public init(eventType: String, eventParams: CodexCommandExecutionEventParams) {
+        self.eventType = eventType
+        self.eventParams = eventParams
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case eventType = "event_type"
+        case eventParams = "event_params"
+    }
+}
+
 public struct CodexReviewEventParams: Equatable, Codable, Sendable {
     public let threadID: String
     public let turnID: String
--- Tests/CodexCoreTests/ReviewAnalyticsTests.swift
+++ Tests/CodexCoreTests/ReviewAnalyticsTests.swift
@@ -2,6 +2,102 @@ import CodexCore
 import XCTest
 
 final class ReviewAnalyticsTests: XCTestCase {
+    func testCodexCommandExecutionEventRequestUsesRustWireShape() throws {
+        let actions: [AppServerProtocol.CommandAction] = [
+            .read(command: "cat README.md", name: "README.md", path: "/repo/README.md"),
+            .listFiles(command: "ls Sources", path: "/repo/Sources"),
+            .search(command: "rg TODO", query: "TODO", path: nil),
+            .unknown(command: "swift test")
+        ]
+        let event = CodexCommandExecutionEventRequest(
+            eventType: "codex_command_execution_event",
+            eventParams: CodexCommandExecutionEventParams(
+                base: CodexToolItemEventBase(
+                    threadID: "thread-1",
+                    turnID: "turn-1",
+                    itemID: "item-1",
+                    appServerClient: CodexAppServerClientMetadata(
+                        productClientID: "codex_tui",
+                        clientName: "codex-tui",
+                        clientVersion: "1.2.3",
+                        rpcTransport: .websocket,
+                        experimentalAPIEnabled: true
+                    ),
+                    runtime: CodexRuntimeMetadata(
+                        codexRSVersion: "0.99.0",
+                        runtimeOS: "macos",
+                        runtimeOSVersion: "15.3.1",
+                        runtimeArch: "aarch64"
+                    ),
+                    threadSource: .user,
+                    subagentSource: nil,
+                    parentThreadID: nil,
+                    toolName: "shell",
+                    startedAtMilliseconds: 123_000,
+                    completedAtMilliseconds: 125_000,
+                    durationMilliseconds: 2_000,
+                    executionDurationMilliseconds: 1_900,
+                    reviewCount: 0,
+                    guardianReviewCount: 0,
+                    userReviewCount: 0,
+                    finalApprovalOutcome: .notNeeded,
+                    terminalStatus: .completed,
+                    failureKind: nil,
+                    requestedAdditionalPermissions: false,
+                    requestedNetworkAccess: false
+                ),
+                commandExecutionSource: .agent,
+                exitCode: 0,
+                commandActionCounts: CodexCommandActionCounts(actions: actions)
+            )
+        )
+
+        try XCTAssertJSONObjectEqual(event, [
+            "event_type": "codex_command_execution_event",
+            "event_params": [
+                "thread_id": "thread-1",
+                "turn_id": "turn-1",
+                "item_id": "item-1",
+                "app_server_client": [
+                    "product_client_id": "codex_tui",
+                    "client_name": "codex-tui",
+                    "client_version": "1.2.3",
+                    "rpc_transport": "websocket",
+                    "experimental_api_enabled": true
+                ],
+                "runtime": [
+                    "codex_rs_version": "0.99.0",
+                    "runtime_os": "macos",
+                    "runtime_os_version": "15.3.1",
+                    "runtime_arch": "aarch64"
+                ],
+                "thread_source": "user",
+                "subagent_source": nil,
+                "parent_thread_id": nil,
+                "tool_name": "shell",
+                "started_at_ms": 123_000,
+                "completed_at_ms": 125_000,
+                "duration_ms": 2_000,
+                "execution_duration_ms": 1_900,
+                "review_count": 0,
+                "guardian_review_count": 0,
+                "user_review_count": 0,
+                "final_approval_outcome": "not_needed",
+                "terminal_status": "completed",
+                "failure_kind": nil,
+                "requested_additional_permissions": false,
+                "requested_network_access": false,
+                "command_execution_source": "agent",
+                "exit_code": 0,
+                "command_total_action_count": 4,
+                "command_read_action_count": 1,
+                "command_list_files_action_count": 1,
+                "command_search_action_count": 1,
+                "command_unknown_action_count": 1
+            ]
+        ])
+    }
+
     func testCodexReviewEventRequestUsesRustWireShape() throws {
         let event = CodexReviewEventRequest(
             eventType: "codex_review_event",
@@ -104,6 +200,38 @@ final class ReviewAnalyticsTests: XCTestCase {
                 "session_approval",
                 "exec_policy_amendment",
                 "network_policy_amendment"
+            ],
+            "tool_item_outcomes": [
+                "unknown",
+                "not_needed",
+                "config_allowed",
+                "policy_forbidden",
+                "guardian_approved",
+                "guardian_denied",
+                "guardian_aborted",
+                "user_approved",
+                "user_approved_for_session",
+                "user_denied",
+                "user_aborted"
+            ],
+            "tool_item_statuses": [
+                "completed",
+                "failed",
+                "rejected",
+                "interrupted"
+            ],
+            "tool_item_failure_kinds": [
+                "tool_error",
+                "approval_denied",
+                "approval_aborted",
+                "sandbox_denied",
+                "policy_forbidden"
+            ],
+            "command_execution_sources": [
+                "agent",
+                "user_shell",
+                "unified_exec_startup",
+                "unified_exec_interaction"
             ]
         ])
     }
@@ -130,6 +258,38 @@ final class ReviewAnalyticsTests: XCTestCase {
             .execPolicyAmendment,
             .networkPolicyAmendment
         ]
+        let toolItemOutcomes: [ToolItemFinalApprovalOutcome] = [
+            .unknown,
+            .notNeeded,
+            .configAllowed,
+            .policyForbidden,
+            .guardianApproved,
+            .guardianDenied,
+            .guardianAborted,
+            .userApproved,
+            .userApprovedForSession,
+            .userDenied,
+            .userAborted
+        ]
+        let toolItemStatuses: [ToolItemTerminalStatus] = [
+            .completed,
+            .failed,
+            .rejected,
+            .interrupted
+        ]
+        let toolItemFailureKinds: [ToolItemFailureKind] = [
+            .toolError,
+            .approvalDenied,
+            .approvalAborted,
+            .sandboxDenied,
+            .policyForbidden
+        ]
+        let commandExecutionSources: [CommandExecutionSource] = [
+            .agent,
+            .userShell,
+            .unifiedExecStartup,
+            .unifiedExecInteraction
+        ]
 
         private enum CodingKeys: String, CodingKey {
             case toolKinds = "tool_kinds"
@@ -137,6 +297,10 @@ final class ReviewAnalyticsTests: XCTestCase {
             case triggers
             case statuses
             case resolutions
+            case toolItemOutcomes = "tool_item_outcomes"
+            case toolItemStatuses = "tool_item_statuses"
+            case toolItemFailureKinds = "tool_item_failure_kinds"
+            case commandExecutionSources = "command_execution_sources"
         }
     }
 }
